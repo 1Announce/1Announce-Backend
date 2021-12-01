43ec20708dceefb26919bc1fe48f27897b29c7bc
Muaz "SandBeeble" Bourhani <72118686+SandBeeble@users.noreply.github.com>
2021-11-30 21:23:11 -0600

    Update utils.test.js

--- src/utils/utils.test.js
+++ src/utils/utils.test.js
@@ -6,7 +6,7 @@ test('properly convert number to cron from datetime', () =>{
     // if (numberToCron('2001-12-01 12:00:00')===1007229600000){
 
     // }
-    expect(numberToCron('2021-11-30 6:25:00')).toBe('cron(25 12 30 11 ? 2021)')
+    expect(numberToCron('2021-11-30 6:25:00')).toBe('cron(25 6 30 11 ? 2021)')
     
 })
 
@@ -22,6 +22,6 @@ test('properly convert number to cron from date', () =>{
     // if (numberToCron('2001-12-01 12:00:00')===1007229600000){
 
     // }
-    expect(numberToCron('30 Nov 2021')).toBe('cron(0 6 30 11 ? 2021)')
+    expect(numberToCron('30 Nov 2021')).toBe('cron(0 0 30 11 ? 2021)')
     
 })
