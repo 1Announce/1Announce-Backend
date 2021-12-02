d4e75feb8d63dbe7e00d6d5aae6272b5f0544994
SandBeeble <72118686+SandBeeble@users.noreply.github.com>
2021-12-02 15:17:54 -0600

    added test

---
+++ tests/utils.test.js
@@ -0,0 +1,21 @@
+
+const { numberToCron } = require('../src/utils/utils')
+const Utils = require('../src/utils/utils')
+describe('numberToCron Unit Test', () => {
+    test('properly convert number to cron from datetime', () =>{
+        expect(numberToCron('2021-11-30 6:25:00')).toBe('cron(25 12 30 11 ? 2021)')
+        
+    })
+    
+    test('properly convert number to cron from millisecond', () =>{
+        expect(numberToCron(1638296700000)).toBe('cron(25 18 30 11 ? 2021)')
+        
+    })
+    
+    test('properly convert number to cron from date', () =>{
+
+        expect(numberToCron('30 Nov 2021')).toBe('cron(0 6 30 11 ? 2021)')
+        
+    })
+})
+
